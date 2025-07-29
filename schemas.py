397d9ccc05a5ae789e972f1a6618449284eb2327
Gnana Prakash M <gnanaprakash2918@gmail.com>
2025-07-29 14:25:50 +0530

    Added schemas defintion with Base model

---
+++ schemas.py
@@ -0,0 +1,34 @@
+# To define models for validating and parsing request data
+from pydantic import BaseModel
+
+# Schema for registering / creating a new user
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    application_code: str
+    secret_key: str
+
+# Schema for User login authentication requests
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+# Schema for sending JWT / OAuth2 access token back to the client after login
+class TokenResponse(BaseModel):
+    access_token: str
+    # bearer is the standard for OAuth2-style APIs
+    token_type: str = "bearer"
+
+# Schema for creating / registering a new device
+class DeviceCreate(BaseModel):
+    name: str
+    application_code: str
+    secret_key: str
+
+# Schema for response model 
+class DeviceOut(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        orm_mode = True
